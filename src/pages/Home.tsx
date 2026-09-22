32431a1554da8aa5f9b3b0e7e0d78294f1ab984a
KaykeMoraes <moraeskayke01@gmail.com>
2026-09-22 19:19:22 -0300

    feat: create sidebar component and basic home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,15 @@
 import { DarkModeButton } from '../components/DarkModeButton'
+import { SideBar } from '../components/SideBar'
 
 export const Home = () => {
   return (
-    <main className="min-h-screen bg-gray-50 p-4 text-neutral-900 dark:bg-neutral-950 dark:text-gray-100">
-      <DarkModeButton />
-      <p className="mt-4">Welcome to your notes home</p>
-    </main>
+    <>
+      <div className="flex flex-row justify-between m-4">
+        <SideBar />
+      </div>
+      <div className="flex justify-center items-center">
+        <h1 className="text-4xl">Welcome to your notes home</h1>
+      </div>
+    </>
   )
 }
